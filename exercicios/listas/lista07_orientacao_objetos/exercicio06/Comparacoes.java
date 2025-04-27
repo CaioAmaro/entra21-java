9b2745ca65c3bdcf3dc8718e0e0d4b345c465d29
Caio Amaro <caioamarocci@gmail.com>
2025-04-27 14:11:13 -0300

    Finalizando Exercicio 06 - de POO

--- exercicios/listas/lista07_orientacao_objetos/exercicio06/Comparacoes.java
+++ exercicios/listas/lista07_orientacao_objetos/exercicio06/Comparacoes.java
@@ -2,22 +2,126 @@ package lista07_orientacao_objetos.exercicio06;
 
 public class Comparacoes {
 
-	public static Mercado maiorVendendorMaca(Mercado vetor[]) {
+	public static Mercado getMaiorVendendorMaca(Mercado[] vetor) {
 		
 		Mercado mercado = new Mercado();
+		double receitaMaca = Double.MIN_VALUE;
 
-		mercado.qtdVendaMaca = Integer.MIN_VALUE;
-		
-		
+
 		for(int i = 0; i < vetor.length; i++) {
-					
-		
-					
+
+			double receitaComparando = vetor[i].precoMaca * vetor[i].qtdVendaMaca;
+
+			System.out.println(receitaComparando);
+
+			if(receitaComparando > receitaMaca){
+				System.out.println("entrei");
+				mercado = vetor[i];
+				receitaMaca = receitaComparando;
+			}
+
 		}
-		
-	
-		
+
 		return mercado;
 		
 	}
+
+	public static Mercado getMaiorVendendorLaranja(Mercado[] vetor) {
+
+		Mercado mercado = new Mercado();
+		double receitaLaranja = Double.MIN_VALUE;
+
+
+		for(int i = 0; i < vetor.length; i++) {
+
+			double receitaComparando = vetor[i].precoLaranja * vetor[i].qtdVendaLaranja;
+
+			if(receitaComparando > receitaLaranja){
+				mercado = vetor[i];
+				receitaLaranja = receitaComparando;
+			}
+
+		}
+
+		return mercado;
+
+	}
+
+	public static Mercado getMercadoMaiorReceita(Mercado[] vetor){
+		Mercado mercado = new Mercado();
+
+		double receitaTotal = Double.MIN_VALUE;
+
+
+		for(int i = 0; i < vetor.length; i++) {
+
+			double receitaComparando = (vetor[i].precoLaranja * vetor[i].qtdVendaLaranja) + (vetor[i].precoMaca * vetor[i].qtdVendaMaca);
+
+			if(receitaComparando > receitaTotal){
+				mercado = vetor[i];
+				receitaTotal = receitaComparando;
+			}
+
+		}
+
+		return mercado;
+	}
+
+	public static Mercado getMercadoMenorReceita(Mercado[] vetor){
+		Mercado mercado = new Mercado();
+
+		double receitaTotal = Double.MAX_VALUE;
+
+
+		for(int i = 0; i < vetor.length; i++) {
+
+			double receitaComparando = (vetor[i].precoLaranja * vetor[i].qtdVendaLaranja) + (vetor[i].precoMaca * vetor[i].qtdVendaMaca);
+
+			if(receitaComparando < receitaTotal){
+				mercado = vetor[i];
+				receitaTotal = receitaComparando;
+			}
+
+		}
+
+		return mercado;
+	}
+
+	public static Mercado getMercadoMeioReceita(Mercado[] vetor){
+		Mercado mercado = new Mercado();
+		Mercado maiorReceita = getMercadoMaiorReceita(vetor);
+		Mercado menorReceita = getMercadoMenorReceita(vetor);
+
+		for(int i = 0; i < vetor.length; i++){
+			if( !vetor[i].nome.equals(maiorReceita.nome) && !vetor[i].nome.equals(menorReceita.nome) ){
+				mercado = vetor[i];
+			}
+		}
+		return mercado;
+	}
+
+	public static String getProdutoMaiorLucro(Mercado[] vetor){
+		String produto = "";
+		double laranja = 0;
+		double maca = 0;
+
+		for(int i = 0; i < vetor.length; i++){
+
+			laranja = vetor[i].precoLaranja * vetor[i].qtdVendaLaranja;
+			maca = vetor[i].precoMaca * vetor[i].qtdVendaMaca;
+
+		}
+
+		if(laranja > maca){
+			produto += "Laranja";
+		}else{
+			produto += "Maça";
+		}
+
+		return produto;
+	}
+
 }
+
+
+         
--- exercicios/listas/lista07_orientacao_objetos/exercicio06/Main.java
+++ exercicios/listas/lista07_orientacao_objetos/exercicio06/Main.java
@@ -1,7 +1,5 @@
 package lista07_orientacao_objetos.exercicio06;
 
-import lista07_orientacao_objetos.exercicio02.Mercado;
-
 /*
  * 6)
 Utilizando a classe Mercado.
@@ -27,24 +25,28 @@ public class Main {
 		Mercado vetor[] = {unidadeDeBlumenau, unidadeDeJoinville, unidadeDeFlorianopolis};
 		
 		vetor[0].nome = "Blumenau";
-		vetor[0].qtdVendaMaca = 3;
-		vetor[0].precoMaca = 3.5;
-		vetor[0].qtdVendaLaranja = 5;
-		vetor[0].precoLaranja = 3.4;
+		vetor[0].qtdVendaMaca = 1;
+		vetor[0].precoMaca = 1;
+		vetor[0].qtdVendaLaranja = 1;
+		vetor[0].precoLaranja = 1;
 		
 		vetor[1].nome = "Joinville";
-		vetor[1].qtdVendaMaca = 2;
-		vetor[1].precoMaca = 3.1;
-		vetor[1].qtdVendaLaranja = 9;
-		vetor[1].precoLaranja = 3.1;
+		vetor[1].qtdVendaMaca = 11;
+		vetor[1].precoMaca = 1;
+		vetor[1].qtdVendaLaranja = 1;
+		vetor[1].precoLaranja = 1;
 		
 		vetor[2].nome = "Florianopolis";
-		vetor[2].qtdVendaMaca = 1;
-		vetor[2].precoMaca = 34;
-		vetor[2].qtdVendaLaranja = 2;
-		vetor[2].precoLaranja = 5.2;
-		
-		
-		
+		vetor[2].qtdVendaMaca = 10;
+		vetor[2].precoMaca = 1;
+		vetor[2].qtdVendaLaranja = 1;
+		vetor[2].precoLaranja = 1;
+
+		System.out.println("Maior vendendor de maça é " + Comparacoes.getMaiorVendendorMaca(vetor).nome );
+		System.out.println("Maior vendendor de Laranja é " + Comparacoes.getMaiorVendendorLaranja(vetor).nome );
+		System.out.println("Maior receita foi " + Comparacoes.getMercadoMaiorReceita(vetor).nome);
+		System.out.println("Menor receita foi " + Comparacoes.getMercadoMenorReceita(vetor).nome);
+		System.out.println("Meio receita foi " + Comparacoes.getMercadoMeioReceita(vetor).nome);
+		System.out.println("Produto que teve mais receita foi " + Comparacoes.getProdutoMaiorLucro(vetor));
 	}
 }
